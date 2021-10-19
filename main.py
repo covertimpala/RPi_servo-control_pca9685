0ec7b3f9fa4d1058d4838beb12822d84ea1a5593
covertimpala <77851547+covertimpala@users.noreply.github.com>
2021-10-19 15:57:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
-import subprocess                  #not required
+import subprocess
 import board
-import busio                       #not required
+import busio
 i2c = busio.I2C(board.SCL, board.SDA)
 from adafruit_servokit import ServoKit
 from multiprocessing import Pool
@@ -13,21 +13,19 @@ import adafruit_motor.servo
 # 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
 kit = ServoKit(channels=16)
 kit.servo.frequency = 50
-kit.servo[0].actuation_range = 180  #sets the max angle on pin 0 to 180°
+kit.servo[0].actuation_range = 180
 kit.servo[1].actuation_range = 120
 kit.servo[2].actuation_range = 130
 kit.servo[3].actuation_range = 180  #still needs adjustment
 kit.servo[4].actuation_range = 180  #still needs adjustment
-kit.servo[5].actuation_range = 25   #range 25 = closed 15 = open
+kit.servo[5].actuation_range = 50   #range 50 = closed 40 = open
 run = 0
-print("select a pin")
 while run <= 100:
     key = input()
     if key == "0":
         print("choose angle")
-        angle = int(input())        #sets var angle to the int of the input
-        kit.servo[0].angle = angle  #moves servo 0 to the value of variable angle°
-        print("select a pin")
+        angle = int(input())
+        kit.servo[0].angle = angle
         
 
     else:
@@ -35,56 +33,58 @@ while run <= 100:
             print("choose angle")
             angle = int(input())
             kit.servo[1].angle = angle
-            print("select a pin")
 
         else:
             if key == "exit":
                 print("exiting")
-                
                 exit()
 
             else:
                 if key == "idle":
                     print("going to idle pos")
                     kit.servo[0].angle = 180
+                    time.sleep(2)
                     kit.servo[1].angle = 30
+                    time.sleep(2)
                     kit.servo[2].angle = 20
+                    time.sleep(2)
                     kit.servo[3].angle = 80
+                    time.sleep(1)
+                    kit.servo[3].angle = 80
+                    time.sleep(2)
                     kit.servo[4].angle = 140
-                    kit.servo[5].angle = 24
+                    time.sleep(2)
+                    kit.servo[5].angle = 50
+                    print("arm has been returned to idle pos")
 
                 else:
                     if key == "2":
                         print("choose angle")
                         angle = int(input())
                         kit.servo[2].angle = angle
-                        print("select a pin")
 
                     else:
                         if key == "3":
                             print("choose angle")
                             angle = int(input())
                             kit.servo[3].angle = angle
-                            print("select a pin")
 
                         else:
                             if key == "4":
                                 print("choose angle")
                                 angle = int(input())
                                 kit.servo[4].angle = angle
-                                print("select a pin")
 
                             else:
                                 if key == "5":
                                     print("choose angle")
                                     angle = int(input())
                                     kit.servo[5].angle = angle
-                                    print("select a pin")
 
                                 else:
-                                    if key == "test":         #This part is just experimentation
-                                        import testsubprocess #runs file testsubprocess
-                                        import testsub2       #runs fie testsub2
+                                    if key == "test":
+                                        import testsubprocess
+                                        import testsub2
                                         
 
 time.sleep(1)
