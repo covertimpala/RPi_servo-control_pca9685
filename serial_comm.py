9e87e33ba0738f32e6d48d7b7d68f2ea4d43815e
covertimpala <77851547+covertimpala@users.noreply.github.com>
2021-11-23 20:57:47 +0000

    Update serial_comm.py

--- serial_comm.py
+++ serial_comm.py
@@ -2,10 +2,14 @@ import serial
 
 
 ser=serial.Serial("/dev/ttyUSB0",9600)  #change ACM number as found from ls /dev/tty/ACM*
-
+disallowed_characters = "rn\'"
 
 while True:
     if(ser.in_waiting > 0):
         line = ser.readline()
-        print(line)
+        firs = str(line).replace("b'","")
+        sec = str(firs).replace("r","")
+        thir = str(sec).replace("\\n'","")
+        final = str(thir).replace("\\","")
+        print(final)
         
