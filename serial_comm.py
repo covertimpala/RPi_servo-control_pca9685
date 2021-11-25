39aaea8805e85385c90c0aee28f2ce54bf9176e9
covertimpala <77851547+covertimpala@users.noreply.github.com>
2021-11-25 22:07:43 +0000

    Update serial_comm.py

--- serial_comm.py
+++ serial_comm.py
@@ -1,19 +1,18 @@
-import serial
-import time
 
+import serial
 
 ser=serial.Serial("/dev/ttyUSB0",9600)  #change ACM number as found from ls /dev/tty/ACM*
-time.sleep(3)
+def sensors():
+    line = ser.readline()  #reads serial communications
+    firs = str(line).replace("b'","")   #removing characters from data
+    sec = str(firs).replace("r","")     #
+    thir = str(sec).replace("\\n'","")  #
+    final = str(thir).replace("\\","")  #
+    processed = final.split(',')
+    global sens0
+    global sens1
+    sens0 = processed[0]
+    sens1 = processed[1]
 
-while True:
-    if(ser.in_waiting > 0):
-        line = ser.readline()  #reads serial communications
-        firs = str(line).replace("b'","")   #removing characters from data
-        sec = str(firs).replace("r","")     #
-        thir = str(sec).replace("\\n'","")  #
-        final = str(thir).replace("\\","")  #
-        processed = final.split(',')
-        sens0 = processed[0]
-        sens1 = processed[1]
         
         
