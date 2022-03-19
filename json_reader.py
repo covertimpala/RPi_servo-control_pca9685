ccaca33ff3ef4991c3dba0e5ca82d853f97bfef8
covertimpala <77851547+covertimpala@users.noreply.github.com>
2022-03-19 08:15:19 +0000

    part of the improvement update
    bugs fixed

--- json_reader.py
+++ json_reader.py
@@ -2,12 +2,12 @@ import json
 import __main__
 
 
-def readfile():
+def readfile(reff):
     with open(__main__.jsonfile, 'r+') as f:  # with open('data.json', 'r+') as f:
         data = json.load(f)
         f.seek(0)
 
-    print("ref num:", __main__.ref)
+    print("ref num:", reff) #__main__.ref
     global val
-    val = data[str(__main__.ref)]   # data extraction using ref string
+    val = data[str(reff)]   # data extraction using ref string
     print(val)
