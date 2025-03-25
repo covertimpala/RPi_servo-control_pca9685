5c7a62ea934629f6e405e89bb430687cfc3d7b32
Eoin Mikael Herron <77851547+covertimpala@users.noreply.github.com>
2025-03-25 11:28:42 +0100

    Update modified.py

--- modified.py
+++ modified.py
@@ -151,9 +151,9 @@ def calculateab(locx, locy, o, _range, bypass, x_dist, y_dist):
             # Verification of the output values
             if abs(verify(a,b,ct)[0] - round(x_dist,2)) <= TOLERANCE and abs(verify(a,b,ct)[1] - round(y_dist,2)) <= TOLERANCE:
                 return([a,b,ct])
-            elif verify(a,b,-ct)[0] == round(x_dist,2) and verify(a,b,-ct)[1] == round(y_dist,2):
+            elif abs(verify(a,b,-ct)[0] - round(x_dist,2)) <= TOLERANCE and abs(verify(a,b,-ct)[1] - round(y_dist,2)) == TOLERANCE:
                 return([a,b,-ct])
-            elif verify(a,b,cp)[0] == round(x_dist,2) and verify(a,b,cp)[1] == round(y_dist,2):
+            elif abs(verify(a,b,cp)[0] - round(x_dist,2)) == TOLERANCE and abs(verify(a,b,cp)[1] - round(y_dist,2)) == TOLERANCE:
                 return([a,b,cp])
             elif verify(a,b,-cp)[0] == round(x_dist,2) and verify(a,b,-cp)[1] == round(y_dist,2):
                 return([a,b,-cp])
